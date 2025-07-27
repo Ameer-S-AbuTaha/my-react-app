7264858ae00cfb1b4c75b83db0f37a98a9904090
AmeerAbuTaha <32480001+AmeerAbuTaha@users.noreply.github.com>
2025-07-27 21:31:39 +0300

    update signup page

--- src/SignUpPage.js
+++ src/SignUpPage.js
@@ -15,7 +15,7 @@ export default function SignUpPage() {
     setSignUpError("");
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/signup", {
+      const res = await fetch("http://127.0.0.1:5000/users", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
